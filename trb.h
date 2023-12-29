5dde221aed8400ce349ecd734f00e3b19ced15a7
Fedor <bfm_2003@mail.ru>
2023-12-29 14:50:31 +0300

    trb complete

--- trb.h
+++ trb.h
@@ -228,6 +228,248 @@ struct MFINDEXWrapEventTRB {
     uint16_t rsvdz2;
 } PACKED;
 
+
 // Command TRBs (6.4.3)
 
-// TRB TYPES DEFINITIION table 6-91
+struct NoOpCommandTRB {
+    uint32_t rsvdz0[3];
+    
+    bool c: 1;
+    uint16_t rsvdz1: 9;
+    uint8_t type: 6;
+    uint8_t slot_type: 5
+    uint16_t rsvdz2: 11;
+} PACKED;
+
+struct EnableSlotCommandTRB {
+    uint32_t rsvdz0[3];
+
+    bool c: 1;
+    uint16_t rsvdz1: 9;
+    uint8_t type: 6;
+    uint16_t rsvdz2;
+} PACKED;
+
+struct DisableSlotCommandTRB {
+    uint32_t rsvdz[3];
+
+    bool c: 1;
+    uint16_t rsvdz1: 9;
+    uint8_t type: 6;
+    uint8_t rsvdz2;
+    uint8_t slot_id;
+} PACKED;
+
+struct AddressDeviceCommandTRB {
+    uint8_t rsvdz0: 4;
+    uint64_t input_context_ptr: 60;
+    
+    uint32_t rsvdz1;
+
+    bool c: 1;
+    uint8_t rsvdz2;
+    bool bsr: 1;
+    uint8_t type: 6;
+    uint8_t rsvdz3;
+    uint8_t slot_id;
+} PACKED;
+
+struct ConfigureEndpointCommandTRB {
+    uint8_t rsvdz0: 4;
+    uint64_t input_context_ptr: 60;
+
+    uint32_t rsvdz1;
+
+    bool c: 1;
+    uint8_t rsvdz2;
+    bool dc: 1;
+    uint8_t type: 6;
+    uint8_t rsvdz3;
+    uint8_t slot_id;
+} PACKED;
+
+struct EvaluateContextCommandTRB {
+    uint8_t rsvdz0: 4;
+    uint64_t input_context_ptr: 60;
+    
+    uint32_t rsvdz1;
+
+    bool c: 1;
+    uint8_t rsvdz2;
+    bool bsr: 1;
+    uint8_t type: 6;
+    uint8_t rsvdz3;
+    uint8_t slot_id;
+} PACKED;
+
+struct ResetEndpointCommandTRB {
+    uint32_t rsvdz0[3];
+
+    bool c: 1;
+    uint8_t rsvdz1;
+    bool tsp: 1;
+    uint8_t type: 6;
+    uint8_t endpoint_id: 5;
+    uint8_t rsvdz2: 3;
+    uint8_t slot_id;
+} PACKED;
+
+struct StopEndpointCommandTRB {
+    uint32_t rsvdz0[3];
+
+    bool c: 1;
+    uint16_t rsvdz1: 9;
+    uint8_t type: 6;
+    uint8_t endpoint_id: 5;
+    uint8_t rsvdz2: 2;
+    bool sp: 1;
+    uint8_t slot_id;
+} PACKED;
+
+struct SetTRDequeuePointerCommandTRB {
+    bool dcs: 1;
+    uint8_t sct: 3;
+    uint64_t new_tr_dequeue_ptr: 60;
+
+    uint16_t rsvdz0;
+    uint16_t stream_id;
+
+    bool c: 1;
+    uint16_t rsvdz1: 9;
+    uint8_t type: 6;
+    uint8_t endpoint_id: 5;
+    uint8_t rsvdz2: 3;
+    uint8_t slot_id;
+} PACKED;
+
+struct ResetDeviceCommandTRB {
+    uint32_t rsvdz0[3];
+
+    bool c: 1;
+    uint16_t rsvdz1: 9;
+    uint8_t type: 6;
+    uint8_t rsvdz2;
+    uint8_t slot_id;
+} PACKED;
+
+struct ForceEventCommandTRB {
+    uint8_t rsvdz0: 4;
+    uint64_t event_trb_ptr: 60;
+
+    uint32_t rsvdp1: 22;
+    uint16_t vf_interrupter_target: 10;
+
+    bool c: 1;
+    uint16_t rsvdz1: 9;
+    uint8_t type: 6;
+    uint8_t vf_id;
+    uint8_t rsvdz2;
+} PACKED;
+
+struct NegotiateBandwidthCommandTRB{
+    // Optional
+} PACKED;
+
+struct SetLTVCommandTRB{
+    // Optional
+} PACKED;
+
+struct GetPortBandwidthCommandTRB {
+    uint8_t rsvdz0: 4;
+    uint64_t port_bandwidth_context_ptr: 60;
+
+    uint32_t rsvdz1;
+
+    bool c: 1;
+    uint16_t rsvdz2: 9;
+    uint8_t type: 6;
+    uint8_t dev_speed: 4;
+    uint8_t rsvdz3: 4;;
+    uint8_t hub_slot_id;
+} PACKED;
+
+struct ForceHeaderCommandTRB {
+    uint8_t packet_type: 5;
+    uint32_t header_info_lo: 27;
+    uint32_t header_info_mid
+    uint32_t header_info_hi;
+
+    bool c: 1;
+    uint16_t rsvdz: 9;
+    uint8_t type: 6;
+    uint8_t rsvdz2;
+    uint8_t root_hub_port_number;
+} PACKED;
+
+struct GetExtendedProperyCommandTRB {
+    uint8_t rsvdz0: 4;
+    uint64_t extended_property_context_ptr: 60;
+
+    uint32_t eci;
+    uint32_t rsvdz1;
+
+    bool c: 1;
+    uint16_t rsvdz2: 9;
+    uint8_t type: 6;
+    uint8_t command_sub_type: 3;
+    uint8_t endpoint_id: 5;
+    uint8_t slot_id;
+} PACKED;
+
+struct SetExtendedProperyCommandTRB {
+    uint32_t rsvdz0[2];
+
+    uint16_t eci;
+    uint16_t capability_parameter: 9;
+    uint8_t: reserved;
+
+    bool c: 1;
+    uint16_t rsvdz1: 9;
+    uint8_t type: 6;
+    uint8_t command_sub_type: 3;
+    uint8_t endpoint_id: 5;
+    uint8_t slot_id;
+} PACKED;
+
+
+// Other TRB's (6.4.4)
+
+struct LinkTRB {
+    uint8_t rsvdz0: 4;
+    uint64_t ring_segment_ptr: 60;
+
+    uint32_t rsvdz1: 22;
+    uint16_t interrupter_target: 10;
+
+    bool c: 1;
+    bool tc: 1;
+    uint8_t rsvdz2: 2;;
+    bool ch: 1;
+    bool ioc: 1;
+    uint8_t rsvdz3: 4;
+    uint8_t type: 6;
+    uint16_t rsvdz4;
+} PACKED;
+
+struct EventDataTRB {
+    uint64_t event_data;
+
+    uint32_t rsvdz0: 22;
+    uint16_t interrupter_target: 10;
+
+    bool c: 1;
+    bool ent: 1;
+    uint8_t rsvdz1: 2;;
+    bool ch: 1;
+    bool ioc: 1;
+    uint8_t rsvdz2: 3;
+    bool bei: 1;
+    uint8_t type: 6;
+    uint16_t rsvdz3;
+} PACKED;
+
+
+
+// TRB Completion Codes (6.4.5)
+
+// TRB Types (6.4.6)
